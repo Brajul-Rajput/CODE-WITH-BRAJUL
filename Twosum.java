87cfc7ca2299a48f29d436e308a833fef6cacd84
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-01-11 16:22:16 +0530

    Create Twosum.java

---
+++ Twosum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Twosum {
+    public static void main(String[]args){
+        Scanner A=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num1=A.nextInt();
+        System.out.println("Enter the number :");
+        int num2=A.nextInt();
+        int sum=num1+num2;
+        System.out.println("THE RESULTS IS : "+ sum);
+    }
+}
