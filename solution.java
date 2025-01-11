71b211829c7d66a570fbc47bc4aa2ec4c83785a4
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-01-11 17:18:53 +0530

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class solution {
+	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		  int p=sc.nextInt();
+		float r=sc.nextFloat();
+        int t=sc.nextInt();
+double si= (p*r*t)/100;
+int xc=(int)si;
+System.out.println(xc);
+		
+	}
+}
+
+ 
